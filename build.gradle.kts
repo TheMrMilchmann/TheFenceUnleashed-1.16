21196fabb11c054655cd8a3adc70340a25a48624
Leon Linhart <themrmilchmann@gmail.com>
2022-08-28 14:52:40 +0200

    chore: bump version number {1.0.0-1.16.5-2 => 1.0.1-1.16.5-1.0}

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ plugins {
 }
 
 group = "com.github.themrmilchmann.fency"
-val nextVersion = "1.0.0-1.16.5-2"
+val nextVersion = "1.0.1-1.16.5-1.0"
 version = when (deployment.type) {
     BuildType.SNAPSHOT -> "$nextVersion-SNAPSHOT"
     else -> nextVersion
